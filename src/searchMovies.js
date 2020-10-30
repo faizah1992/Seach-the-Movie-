c96218a54bd40f2e600f869fb6f930ce6fbe7273
faizah1992 <faizah1992@gmail.com>
2020-10-30 10:27:32 -0500

    added api key

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './index.css'
 
 export default function SearchMovies(){
+
+    const [query, setQuery] = useState('')
+
+    const searchMovies = async (e) => {
+        e.preventDefault()
+        
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=0c6aa946be292140a01634e05efeda91&language=en-US&query=${query}&page=1&include_adult=false`
+    
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data);
+        }catch(err){
+            console.error(err);
+        }
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query" placeholder="i.e. Jurassic Park"/>
+            <input className="input" type="text" name="query" placeholder="i.e. Jurassic Park" value={query} onChange={(e) => setQuery(e.target.value)}/>
             <button className="button" type="submit">Search</button>
         </form>
     )
